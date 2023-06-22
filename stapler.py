262e9f9b126f1c8bc6886935273d6466d5e4ba3c
Christopher Johnston <106709685+crj0139@users.noreply.github.com>
2023-06-21 20:50:18 -0500

    Update stapler.py

--- stapler.py
+++ stapler.py
@@ -1,3 +1,3 @@
-#Stapler is a tool for mapping SRA reads to reference genomes and quickly appending coordinate info to reads for downstream graphing
-
+# Stapler is a tool for mapping SRA reads to reference genomes and quickly appending coordinate info to reads for downstream graphing
+# To do - single empty > at beginning of finished reads (line 1)
 import os
