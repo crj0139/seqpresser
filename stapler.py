27afa7861b64b805b0bb728a5a50c08bc0712deb
Christopher Johnston <106709685+crj0139@users.noreply.github.com>
2023-06-21 22:23:42 -0500

    Update stapler.py

--- stapler.py
+++ stapler.py
@@ -1,3 +1,9 @@
 # Stapler is a tool for mapping SRA reads to reference genomes and quickly appending coordinate info to reads for downstream graphing
 # To do - single empty > at beginning of finished reads (line 1)
+import subprocess
 import os
+
+repodir = os.getcwd()
+
+seqtk = "seqtk seq -A sample.fastq > sample.fasta" 
+subprocess.run(command, shell=True, cwd=repodir)
