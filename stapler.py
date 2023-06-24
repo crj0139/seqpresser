df5a1c2b86364a9e4e9073ad9b28730e7ace94c5
Christopher Johnston <106709685+crj0139@users.noreply.github.com>
2023-06-24 08:44:56 -0500

    Update stapler.py

--- stapler.py
+++ stapler.py
@@ -1,17 +1,40 @@
-# Stapler is a tool for mapping SRA reads to reference genomes and quickly appending coordinate info to reads for downstream graphing
-# To do - single empty > at beginning of finished reads (line 1)
-import subprocess
 import os
+import argparse
 
-repodir = os.getcwd()
+def process_sra(sra_number):
+    # Implement SRA processing logic here
+    pass
 
+def process_input_genome(input_genome):
+    # Implement input genome processing logic here
+    pass
 
+def generate_output(output_prefix, output_format):
+    # Implement output file generation logic here
+    pass
 
-# INPUT 1 - sample.fastq (or .fasta if no need for conversion)
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Stapler program")
+    parser.add_argument('-s', '--sra', help='SRA number')
+    parser.add_argument('-i', '--input', help='Input genome file')
+    parser.add_argument('-f', '--format', help='Output format (1, 2, or 3)')
+    parser.add_argument('-o', '--output', help='Output prefix')
+    args = parser.parse_args()
 
-seqtk_path = "/path/to/seqtk"
-input_file = "sample.fastq"
-output_file = "sample.fasta"
+    # Process the input based on the provided arguments
+    if args.sra:
+        process_sra(args.sra)
+    elif args.input:
+        process_input_genome(args.input)
+    else:
+        print("Please provide either -s/--sra or -i/--input argument.")
 
-convert = "seqtk seq -A sample.fastq > sample.fasta" 
-subprocess.run(convert, shell=True, cwd=repodir)
+    # Generate the output file
+    if args.output and args.format:
+        generate_output(args.output, args.format)
+    else:
+        print("Please provide both -f/--format and -o/--output arguments.")
+
+if __name__ == '__main__':
+    main()
