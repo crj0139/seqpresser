05db8edbb8eaef09eb5ff094624fda948d5bbcee
Christopher Johnston <106709685+crj0139@users.noreply.github.com>
2023-07-12 17:03:02 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,14 +17,7 @@ fi
 
 https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/3.0.5/sratoolkit.3.0.5-ubuntu64.tar.gz
 
-# git dependencies seqrequester/build/bin/seqrequester
-if ! command -v prefetch &> /dev/null; then
-    echo "prefetch not found in PATH. Installing SRA tools..."
-    wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/3.0.5/sratoolkit.3.0.5-ubuntu64.tar.gz
-    cd seqrequester/src
-    make
-    cd ../../
-    echo "Installation complete!"
-else
-    echo "Unable to install read simulator"
-fi
+# sra...
+#if ! command -v prefetch &> /dev/null; then
+#    echo "prefetch not found in PATH. Installing SRA tools..."
+#    wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/3.0.5/sratoolkit.3.0.5-ubuntu64.tar.gz
